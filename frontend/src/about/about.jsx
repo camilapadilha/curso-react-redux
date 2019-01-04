eb1018b8ab008976375fc1ed579cdcbf042b1cc4
camilapadilha <camilapadilha1997@gmail.com>
2019-01-04 11:22:28 -0200

    Adição do frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Texto qualquer</p>
+        <h2>Missão e Visão</h2>
+        <p>Texto qualquer aqui também</p>
+        <h2>Imprensa</h2>
+        <p>Texto qualquer aqui também</p>
+    </div>
+)
